58d6f6aae9b7cec6c01bc5beceaac73a40289469
Ryan Luu <ryanluu12345@gmail.com>
2019-04-21 23:32:33 -0700

    Added character countdown feature

--- Twitter/Controllers/TweetViewController.swift
+++ Twitter/Controllers/TweetViewController.swift
@@ -8,12 +8,39 @@
 
 import UIKit
 
-class TweetViewController: UIViewController {
+class TweetViewController: UIViewController, UITextViewDelegate {
     @IBOutlet weak var tweetTextView: UITextView!
+    @IBOutlet weak var characterCountLabel: UILabel!
+    
+    let characterLimit: Int = 140
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        setTextViewBorder()
         tweetTextView.becomeFirstResponder()
+        tweetTextView.delegate = self
+    }
+    
+    func setTextViewBorder() {
+        tweetTextView.layer.borderWidth = 1
+        tweetTextView.layer.cornerRadius = 5
+        tweetTextView.layer.borderColor = UIColor.black.cgColor
+    }
+    
+    func textView(_ textView: UITextView, shouldChangeTextIn range: NSRange, replacementText text: String) -> Bool {
+        
+        // Construct what the new text would be if we allowed the user's latest edit
+        let newText = NSString(string: textView.text!).replacingCharacters(in: range, with: text)
+        
+        // TODO: Update Character Count Label
+        characterCountLabel.text = String(characterLimit - newText.count) + "/" + String(characterLimit)
+        
+        if (newText.characters.count > characterLimit) {
+            tweetTextView.layer.borderColor = UIColor.red.cgColor
+        }
+        
+        // The new text should be allowed? True/False
+        return newText.characters.count < characterLimit
     }
     
     @IBAction func onCancelTap(_ sender: Any) {
